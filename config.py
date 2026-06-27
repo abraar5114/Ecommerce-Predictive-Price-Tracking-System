73185b4267db29061706b340b534c184ab305402
Rehaan <rehaanmohammed697@gmail.com>
2026-06-27 06:38:16 +0530

    fix config for render

--- config.py
+++ config.py
@@ -3,18 +3,20 @@
 # Configuration File
 # =============================================
 
+import os
+
 # --- ScraperAPI Key ---
-SCRAPER_API_KEY = "f4952c74f509965964a0a6d45d601127"
+SCRAPER_API_KEY = os.environ.get("SCRAPER_API_KEY", "f4952c74f509965964a0a6d45d601127")
 
 # --- Email Settings ---
-SENDER_EMAIL = "shaikabraar6009@gmail.com"
-SENDER_APP_PASSWORD = "goqq dxns dizu grew"
+SENDER_EMAIL = os.environ.get("SENDER_EMAIL", "shaikabraar6009@gmail.com")
+SENDER_APP_PASSWORD = os.environ.get("SENDER_APP_PASSWORD", "goqq dxns dizu grew")
 
 # --- Database ---
 DATABASE_NAME = "ecommerce_predictive_price_tracking_system.db"
 
 # --- Scraping Settings ---
-CHECK_INTERVAL_HOURS = 24
+CHECK_INTERVAL_HOURS = 6
 PRICE_DROP_ALERT_PERCENTAGE = 20
 
 # --- Sites To Scrape ---
